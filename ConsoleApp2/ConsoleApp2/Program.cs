40c40970b3a3f670f19cdc0af234539fcfc0cbb5
Thushara_thilan <142692846+LUC1f3R-0@users.noreply.github.com>
2025-09-03 14:04:08 +0530

    name

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nikesh Thushan");
+
+            String fName = "Nikesh";
+            String lName = "thushan";
+
+            Console.WriteLine($"{fName} {lName}");
+
+            Console.ReadLine();
+        }
+    }
+}
